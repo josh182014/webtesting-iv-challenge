8a8031ad46a0de5fa8f1cf3ba3d490a6c6c79427
josh182014 <josh182014@gmail.com>
2019-06-20 13:39:34 -0600

    created knexfile

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+const dbConnnection = process.env.DATABASE_URL;
+
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/database.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: dbConnnection,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+};
